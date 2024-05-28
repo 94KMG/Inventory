f6c0f88a107635163df3009510ca07dfb773f924
94KMG <dnjsxn26@gmail.com>
2024-05-28 13:50:56 +0900

    RoomDatabase 클래스 이름 변경

--- app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 //데이터베이스에 포함될 엔티티(테이블)를 지정합니다.
 @Database(entities = [Item::class], version = 1, exportSchema = false)
-abstract class InventoryDatabase : RoomDatabase() {
+abstract class ItemRoomDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
 
     //클래스의 정적 멤버처럼 동작하는 객체를 정의합니다.
@@ -16,17 +16,18 @@ abstract class InventoryDatabase : RoomDatabase() {
         //변수를 휘발성으로 선언합니다. 이는 여러 스레드에서 변수의 최신 값을 즉시 읽고 쓸 수 있도록 보장합니다.
         // 따라서, 한 스레드가 Instance를 변경하면 다른 스레드에서도 즉시 변경된 값을 볼 수 있습니다.
         @Volatile
-        private var Instance: InventoryDatabase? = null
-        fun getDatabase(context: Context): InventoryDatabase {
+        private var Instance: ItemRoomDatabase? = null
+        fun getDatabase(context: Context): ItemRoomDatabase {
             //Instance가 null이면 synchronized 블록 내부의 코드를 실행합니다
             return Instance ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    InventoryDatabase::class.java,
+                    ItemRoomDatabase::class.java,
                     "item_database"
                 )
                     .fallbackToDestructiveMigration()
                     .build()
+
                     //생성된 인스턴스를 Instance 변수에 저장합니다.
                     Instance = instance
                     // 이 후, Instance가 null이 아니게 되어 다음 호출부터는 이미 생성된 인스턴스를 반환합니다.
