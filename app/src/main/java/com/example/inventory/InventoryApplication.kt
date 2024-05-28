b9dacc2594967814fdcdeaa7c092193d0f5f9429
94KMG <dnjsxn26@gmail.com>
2024-05-28 13:53:41 +0900

    Application 클래스 구현

--- app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -17,18 +17,13 @@
 package com.example.inventory
 
 import android.app.Application
-import com.example.inventory.data.AppContainer
-import com.example.inventory.data.AppDataContainer
+import com.example.inventory.data.ItemRoomDatabase
 
 class InventoryApplication : Application() {
 
     /**
      * AppContainer instance used by the rest of classes to obtain dependencies
      */
-    lateinit var container: AppContainer
-
-    override fun onCreate() {
-        super.onCreate()
-        container = AppDataContainer(this)
-    }
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
 }
+
